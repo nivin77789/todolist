3d8816fd569f4e54f2c79fecd860aa0146baecf4
Nivin Mathew S <nivin77789@gmail.com>
2023-05-22 22:12:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [username, setUsername] = useState("nivin77789");
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    try {
+      axios.get(`https://api.github.com/users/` + username).then((res) => {
+        setUser(res.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [username]);
+  function handleSubmit() {
+    setUsername(input);
+    try {
+      const link = `https://api.github.com/users/` + username;
+      console.log(link);
+      axios.get(link).then((res) => {
+        setUser(res.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <navbar>
+        <div className="nav">
+          <img
+            alt="github"
+            src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_960_720.png"
+          ></img>
+          <h2>Github User Finder</h2>
+        </div>
+      </navbar>
+      <center>
+        <div className="top">
+          <div className="inp">
+            <img
+              alt="s"
+              src="https://github-user-search-app-kzaleskaa.netlify.app/static/media/icon-search.0cc0c984.svg"
+            ></img>
+            <input
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              placeholder="Search Github username.."
+            ></input>
+            <button
+              onClick={() => {
+                handleSubmit();
+              }}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </center>
+      <div className="App">
+        <div className="sub">
+          <div className="left">
+            <img alt="github" src={user.avatar_url}></img>
+            <center>
+              {" "}
+              <h1>{user.name}</h1>
+            </center>
+            <h3>@{user.login}</h3>
+          </div>
+          <div className="right">
+            <h2>Github User Details</h2>
+            <div className="sub-right">
+              <table>
+                <tr>
+                  <th className="th1">Repos</th>
+                  <th className="th2">Followers</th>
+                  <th className="th3">Following</th>
+                </tr>
+                <tr>
+                  <td className="td1">{user.public_repos}</td>
+                  <td className="td2">{user.followers}</td>
+                  <td className="td3">{user.following}</td>
+                </tr>
+              </table>
+              <h3>
+                Since : <span>{user.created_at}</span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
